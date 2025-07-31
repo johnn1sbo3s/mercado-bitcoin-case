0ef339768b9dbb41167fb7ba1a36bd0cf60d6fb9
johnn1sbo3s <joaopaulosncastro@gmail.com>
2025-07-31 19:40:30 -0300

    adiciona lógica de emit ao componente de botão

--- src/js/core/components/FormButton.vue
+++ src/js/core/components/FormButton.vue
@@ -1,12 +1,31 @@
 <template>
   <button
     class="form-button__content"
+    :class="{ 'form-button__content--secondary' : secondary }"
+    @click="handleClick"
   >
-    Avançar
+    {{ text }}
   </button>
 </template>
 
 <script setup>
+
+defineProps({
+  text: {
+    type: String,
+    required: true
+  },
+  secondary: {
+    type: Boolean,
+    default: false
+  }
+});
+
+const emit = defineEmits(['click']);
+
+function handleClick() {
+  emit('click');
+}
 
 </script>
 
@@ -18,11 +37,19 @@
   border-radius: var(--spacing-sm);
   font-weight: bold;
   border: none;
+  cursor: pointer;
 
   &__content {
     @extend .form-button;
-    background-color: var(--color-primary);
+    background: var(--color-primary);
     color: white;
+
+    &--secondary {
+      @extend .form-button;
+      background: var(--color-secondary);
+      color: var(--color-text-primary);
+      border: 1px solid var(--color-border);
+    }
   }
 }
 
